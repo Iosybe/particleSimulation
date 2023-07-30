dc60d3a12ce1d25d24c7fff485b35fd320d44140
Iosybe <finnvancaem@gmail.com>
2023-07-30 16:10:36 +0200

    row based threading

--- src/physics.c
+++ src/physics.c
@@ -16,7 +16,7 @@ float randFloatRange(float min, float max) {
 
 void initializeParticles(Particle* particles) {
     for (int i = 0; i < NOP; i++) {
-        particles[i] = {
+        particles[i] = (Particle) {
             i,
             randFloat(5.0),
 
@@ -26,8 +26,12 @@ void initializeParticles(Particle* particles) {
             0.0,
             0.0,
         };
+
+        pthread_mutex_init(&particles[i].mutex, NULL);
     }
 }
+
+// destroy particles function
 
 // Optimization because pow() is slow
 float powTwo(float n) {
@@ -46,37 +50,61 @@ float timedifference_msec(struct timeval t0, struct timeval t1) {
     return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 }
 
+void* particleInteractions(void* args) {
+    Particle* particles = ((InteractionData*) args)->particles;
+    int i = ((InteractionData*) args)->i;
+
+    for (int j = i + 1; j < NOP; j++) {
+        float diffX = particles[i].posX - particles[j].posX;
+        float diffY = particles[i].posY - particles[j].posY;
+
+        float distance = calcDistance(diffX, diffY);
+
+        if (distance > 50) {
+            float tempGravFactor = 1 / powThree(distance);
+
+            float gravFactorI= -particles[j].mass * tempGravFactor;
+            float gravFactorJ= -particles[i].mass * tempGravFactor;
+
+            pthread_mutex_lock(&particles[i].mutex);
+            particles[i].velX += gravFactorI * diffX;
+            particles[i].velY += gravFactorI * diffY;
+            pthread_mutex_unlock(&particles[i].mutex);
+
+            pthread_mutex_lock(&particles[j].mutex);
+            particles[j].velX -= gravFactorJ * diffX;
+            particles[j].velY -= gravFactorJ * diffY;
+            pthread_mutex_unlock(&particles[j].mutex);
+        }
+    }
+
+    return NULL;
+}
+
 void calculatePhysics(Particle* particles) {
     struct timeval t0;
     struct timeval t1;
     gettimeofday(&t0, 0);
 
-    // Creating buffer arrays
-    // static float gravFactorBuf[BUFSIZE];
-    // static float diffXBuf[BUFSIZE];
-    // static float diffYBuf[BUFSIZE];
+    pthread_t threadIds[NOP];
+    InteractionData interactionDatas[NOP];
 
     for (int i = 0; i < NOP; i++) {
-        for (int j = i + 1; j < NOP; j++) {
-            float diffX = particles[i].posX - particles[j].posX;
-            float diffY = particles[i].posY - particles[j].posY;
+        interactionDatas[i] = (InteractionData) {
+            i,
+            particles,
+        };
 
-            float distance = calcDistance(diffX, diffY);
+        pthread_create(&threadIds[i], NULL, particleInteractions, (void*) (interactionDatas + i));
+    }
 
-            if (distance > 50) {
-                float gravFactor = -particles[j].mass / powThree(distance);
-
-                particles[i].velX += gravFactor * diffX;
-                particles[i].velY += gravFactor * diffY;
-
-                particles[j].velX += gravFactor * -diffX;
-                particles[j].velY += gravFactor * -diffY;
-            }
-        }
+    for (int i = 0; i < NOP; i++) {
+        pthread_join(threadIds[i], NULL);
 
         particles[i].posX += particles[i].velX;
         particles[i].posY += particles[i].velY;
     }
+
 
     gettimeofday(&t1, 0);
     printf("%f\n", timedifference_msec(t0, t1));
--- src/physics.h
+++ src/physics.h
@@ -1,6 +1,8 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
+#include <pthread.h>
+
 #define NOP 5000
 #define BUFSIZE (int) (NOP / 2.0 * (NOP - 1))
 
@@ -13,7 +15,14 @@ typedef struct particle {
 
     float velX;
     float velY;
+
+    pthread_mutex_t mutex;
 } Particle;
+
+typedef struct InteractionData {
+    int i;
+    Particle* particles;
+} InteractionData;
 
 void initializeParticles(Particle* particles);
 void calculatePhysics(Particle* particles);
