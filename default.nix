baa990cd6872b75553b5649f5d8f062f3e7ab42e
Iosybe <finnvancaem@gmail.com>
2023-08-21 12:04:29 +0200

    removed executable and updated nix file

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [ 
+      cmake
       libGL
       xorg.libXrandr 
       xorg.libXinerama
@@ -10,4 +11,10 @@
       xorg.libXi
       xorg.libXxf86vm
     ];
+
+    shellHook = ''
+      alias build="cmake --build build/"
+      alias run="./build/ParticleSimulation"
+    '';
+
 }
